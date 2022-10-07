38d43bcf437c12f1849759b4eaced109278c0e7e
Shanks <onyangogeorge954@gmail.com>
2022-10-07 08:33:25 +0300

    Add functionality

--- src/components/auth/signout.js
+++ src/components/auth/signout.js
@@ -1,24 +1,43 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-
 function SignOut() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  function logout() {
+    navigate("/signin");
+  }
 
-    function logout() {
-        navigate('/signin')
-    }
+  function backToHomePage() {
+    navigate("/");
+  }
 
-    function takeMeHome() {
-        navigate('/')
-    }
-
-    return (
-        <div>
-            <p>Do you wish to leave</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>Do you wish to leave</p>
+      <button
+        style={{
+          borderRadius: "10",
+          color: "#75B8C8",
+          backgroundColor: "#CDD3D5",
+          margin: "0 10px 0 15px",
+          width: "150px",
+          height: "50px",
+        }}
+        onClick={logout}
+      >
+        Sign out
+      </button>
+      <button
+        style={{
+          width: "150px",
+          height: "50px",
+          borderRadius: "10",
+        }}
+        onClick={backToHomePage}
+      ></button>
+    </div>
+  );
 }
 
-export default SignOut
+export default SignOut;
