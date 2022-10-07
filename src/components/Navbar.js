a1c2c60868bf181a93d1bdbf7e8ad001a1899be2
Shanks <onyangogeorge954@gmail.com>
2022-10-07 19:14:52 +0300

    Better decorate the navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const linkStyles = {
-  display: "",
   width: "100px",
   padding: "20px",
   margin: "0 10px 10px",
@@ -14,29 +13,31 @@ const linkStyles = {
 
 function Navbar() {
     return (
-       <div
-      style={{
-        width: "1000px",
-          textAlign: "right",
-          paddingTop: '50px',
-          paddingBottom: '50px',
-          marginRight:'20px'
-      }}
-    >
-      {/* <NavLink to="/signin" exact="true" style={linkStyles}>
-        Sign In
-      </NavLink> */}
-      <NavLink to="/" exact="true" style={linkStyles}>
-         Home
-      </NavLink>
-      <NavLink to="/createstudent" exact="true" style={linkStyles}>
-        Create Student
-      </NavLink>
-      <NavLink to="/signout" exact="true" style={linkStyles}>
-        Sign Out
-      </NavLink>
-    </div> 
-    )
+      <div
+        style={{
+          paddingTop: "50px",
+          paddingBottom: "50px",
+        }}
+      >
+        <NavLink to="/" exact="true" style={linkStyles}>
+          Home
+        </NavLink>
+        <NavLink to="/createstudent" exact="true" style={linkStyles}>
+          Create Student
+        </NavLink>
+        <div style={{
+          paddingLeft:'1600px',
+          
+        }}>
+          <NavLink to="/signin" exact="true" style={linkStyles}>
+            Sign In
+          </NavLink>
+          <NavLink to="/signout" exact="true" style={linkStyles}>
+            Sign Out
+          </NavLink>
+        </div>
+      </div>
+    );
 }
 
 export default Navbar
