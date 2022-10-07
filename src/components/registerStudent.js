cd36d86af87a8ec2cd94c6f89568d5466779f989
Shanks <onyangogeorge954@gmail.com>
2022-10-07 19:26:57 +0300

    Add final styling

--- src/components/registerStudent.js
+++ src/components/registerStudent.js
@@ -40,75 +40,99 @@ function CreateStudent({ addNewStudent }) {
     });
   }
   return (
-    <div>
-      <form className="resume" onSubmit={(e) => createStudent(e)}>
-        {/*official name, phone, email */}
-        <div id="form-detail">
-          <h3>Contact Details</h3>
-          <label htmlFor="name">Name</label>
-          <br />
-          <input
-            style={{ marginBottom: "10px", height: "30px" }}
-            type="text"
-            name="name"
-            value={studentDetails.name}
-            onChange={(e) => handleChange(e)}
-          />
-          <br />
-          <label htmlFor="email">Email</label>
-          <br />
-          <input
-            style={{ marginBottom: "10px", height: "30px" }}
-            type="email"
-            name="email"
-            id="email"
-            value={studentDetails.email}
-            onChange={(e) => handleChange(e)}
-          />
-          <br />
-          <label htmlFor="github">Mobile Number</label>
-          <br />
-          <input
-            style={{ marginBottom: "10px", height: "30px" }}
-            type="number"
-            name="mobile"
-            value={studentDetails.mobile}
-            onChange={(e) => handleChange(e)}
-          />
-        </div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#75B8C8",
+          borderRadius: "20px",
+          width: "500px",
+          display: "flex",
+          justifyContent: "center",
+          opacity: ".9",
+        }}
+      >
+        <form className="resume" onSubmit={(e) => createStudent(e)}>
+          {/*official name, phone, email */}
+          <div id="form-detail">
+            <h3>Contact Details</h3>
+            <label htmlFor="name">Name</label>
+            <br />
+            <input
+              style={{ marginBottom: "10px", height: "30px" }}
+              type="text"
+              name="name"
+              value={studentDetails.name}
+              onChange={(e) => handleChange(e)}
+            />
+            <br />
+            <label htmlFor="email">Email</label>
+            <br />
+            <input
+              style={{ marginBottom: "10px", height: "30px" }}
+              type="email"
+              name="email"
+              id="email"
+              value={studentDetails.email}
+              onChange={(e) => handleChange(e)}
+            />
+            <br />
+            <label htmlFor="github">Mobile Number</label>
+            <br />
+            <input
+              style={{ marginBottom: "10px", height: "30px" }}
+              type="number"
+              name="mobile"
+              value={studentDetails.mobile}
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
 
-        {/* Education */}
-        <div className="education">
-          <h3>Previuos Academic Report</h3>
-          <label htmlFor="education">Most Recent Education level</label>
-          <br />
-          <input
-            style={{ height: "30px" }}
-            type="text"
-            name="education"
-            value={studentDetails.education}
-            onChange={(e) => handleChange(e)}
-          />
-        </div>
+          {/* Education */}
+          <div className="education">
+            <h3>Previuos Academic Report</h3>
+            <label htmlFor="education">Most Recent Education level</label>
+            <br />
+            <input
+              style={{ height: "30px" }}
+              type="text"
+              name="education"
+              value={studentDetails.education}
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
 
-        {/* identity */}
-        <div className="identification">
-          <h3>Form of Identification</h3>
-          <label htmlFor="identification">National ID</label>
-          <br />
-          <input
-            style={{ height: "30px" }}
-            type="number"
-            name="nationalId"
-            id="nationalId"
-            value={studentDetails.nationalId}
-            onChange={(e) => handleChange(e)}
-          />
-        </div>
+          {/* identity */}
+          <div className="identification">
+            <h3>Form of Identification</h3>
+            <label htmlFor="identification">National ID</label>
+            <br />
+            <input
+              style={{ height: "30px", marginBottom: "20px" }}
+              type="number"
+              name="nationalId"
+              id="nationalId"
+              value={studentDetails.nationalId}
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
 
-        {/* Submit button */}
-        <button>Create</button>
-      </form>
+          {/* Submit button */}
+          <button
+            style={{
+              width: "90px",
+              height: "30px",
+              marginBottom: "20px",
+            }}
+          >
+            Create
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
