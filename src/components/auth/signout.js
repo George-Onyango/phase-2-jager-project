bfe91cd7f307db513eca9427ed7cbf017310e35d
Shanks <onyangogeorge954@gmail.com>
2022-10-07 07:57:04 +0300

    Add navigation in the signout

--- src/components/auth/signout.js
+++ src/components/auth/signout.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 
 function SignOut() {
+    const navigate = useNavigate();
 
+
+    function logout() {
+        navigate('/signin')
+    }
+
+    function takeMeHome() {
+        navigate('/')
+    }
+
+    return (
+        <div>
+            <p>Do you wish to leave</p>
+        </div>
+    )
 }
 
 export default SignOut
