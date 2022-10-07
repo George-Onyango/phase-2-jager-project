439936b3d6e3f18394a1675ce698ff8393985124
Shanks <onyangogeorge954@gmail.com>
2022-10-07 10:47:12 +0300

    correct component mistake

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ function App() {
       style={{
         backgroundColor: "#CDD3D5",
         backgroundImage:
-          "url(" +
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRsX3dHA8e7MYPh5Ui-XizCQ_-RAKGj0PEAEh2uA1M0MP5EwNx28Ix9nLPOYLLYkXjYPc&usqp=CAU" + ')',
+          `url(
+          https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRsX3dHA8e7MYPh5Ui-XizCQ_-RAKGj0PEAEh2uA1M0MP5EwNx28Ix9nLPOYLLYkXjYPc&usqp=CAU)`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
         height:'100vh'
