ac0ef33e21792d4e2aedb5f90ac621e3b704142e
Shanks <onyangogeorge954@gmail.com>
2022-10-07 07:28:59 +0300

    Change color styling to hex

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const linkStyles = {
-    display: 'inline-block',
-    width: '100px',
-    padding: "20px",
-    margin: '0 10px 10px',
-    background: "rgb(12 74 110)",
-    textDecoration: 'none',
-    color: 'white',
-    borderRadius: '6px'
+  display: "flex",
+  width: "100px",
+  padding: "20px",
+  margin: "0 10px 10px",
+  background: "#0xff75B8C8",
+  textDecoration: "none",
+  color: "white",
+  borderRadius: "6px",
 };
 
 function Navbar() {
