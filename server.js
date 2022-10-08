074d294d758af2c105528399c239837b98f6c487
Shanks <onyangogeorge954@gmail.com>
2022-10-08 10:00:53 +0300

    Connect to heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+const data = require('./db.json');
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router(data)
+const middlewares = jsonServer.defaults()
+const ports = process.env.PORT || 3000;
+
+
+server.use(middlewares)
+server.use(router)
+
+server.listen(ports)
