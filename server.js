1ed1bb835c9006dcd7d07f16b850d20a6fc44f69
Shanks <onyangogeorge954@gmail.com>
2022-10-08 10:46:38 +0300

    adjust serverjs

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-import data from './db.json';
-
-import { create, router as _router, defaults, rewriter } from 'json-server';
-const server = create()
-const router = _router(data)
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
 const middlewares = defaults({
-    static:'./build'
-})
+  static: "./build",
+});
 const ports = process.env.ports || 3000;
 
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+server.use(router);
 
-server.use(middlewares)
-server.use(jsonServer.rewriter({
-    '/api/*':'/$1'
-}))
-server.use(router)
-
-server.listen(ports)
+server.listen(ports);
