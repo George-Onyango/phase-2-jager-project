5a029484067c6eb9e5a256f688d6bca1c662dbc8
Shanks <onyangogeorge954@gmail.com>
2022-10-08 10:38:14 +0300

    add server hosting

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import data from './db.json';
 
-import { create, router as _router, defaults } from 'json-server';
+import { create, router as _router, defaults, rewriter } from 'json-server';
 const server = create()
 const router = _router(data)
-const middlewares = defaults()
-const ports = process.env.PORT || 3000;
+const middlewares = defaults({
+    static:'./build'
+})
+const ports = process.env.ports || 3000;
 
 
 server.use(middlewares)
+server.use(jsonServer.rewriter({
+    '/api/*':'/$1'
+}))
 server.use(router)
 
 server.listen(ports)
